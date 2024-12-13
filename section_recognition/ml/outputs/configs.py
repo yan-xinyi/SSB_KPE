f06a01f759418d5b262f732b4269806b6b2a4167
Xinyi Yan <140530497+yan-xinyi@users.noreply.github.com>
2024-12-13 15:35:35 +0000

    Delete section_recognition/ml/outputs/configs.py

--- section_recognition/ml/outputs/configs.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2022/4/4 20:14
-# @Author  : leizhao150
-
-COLUMNS = ['p_id', 'label', 'sec_title', 'sec_subtitle',
-        'dataset_id', 'jname_id', 'bib_num', 'fn_num',
-        'fig_num', 'tab_num', 'equ_num', 'para_num', 'sen_num',
-        'word_num', 'sec_loc', 'sec_text']
-LABELS2IDS = {"引言": 0, "相关工作": 1, "方法": 2, "评估与结果":3, "讨论与结论": 4, "其他": 5}
